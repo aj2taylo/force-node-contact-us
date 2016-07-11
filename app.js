1a93844b6195428dbf8abfbd17d62bcb55772320
Andrew Taylor <andrew.taylor@cloudwareconnections.com>
2016-07-11 15:50:46 -0400

    updated README.md; pruned dead (commented out) code

--- app.js
+++ app.js
@@ -40,40 +40,17 @@ hbs.registerHelper('get', function(field) {
 
 
 app.post('/form', function(req, res) {
-    console.log('************************************');
-    console.log('req is');
-    console.log(req.query.code);
-
     // Parse a form
     var form = new formidable.IncomingForm();
 
     form.parse(req, function(err, fields, files) {
-      //res.writeHead(200, {'content-type': 'text/plain'});
-      //res.write('Received form:\n\n');
-      //res.end(util.inspect(fields));
-      var bodyContentA  = util.inspect(fields);
       var bodyContent   = '{ "firstName" : "' + fields.firstName + '", "lastName" :  "' + fields.lastName + '", "email":  "' + fields.email + '", "company" :  "' + fields.company + '", "description" :  "' + fields.description + '" , "type":  "' + fields.type + '" }';
         org.authenticate({ username: process.env.SFUSER, password: process.env.SFPASS, securityToken: process.env.SFTOKEN}, function(err) {
-          console.log('**************************ERR???*******************');
-          console.log(err);
-          console.log('**************************ERR???*******************');
           if (!err) {
-              console.log('**************************bodyContent*******************');
-              console.log(bodyContent);
-              console.log('**************************bodyContent*******************');
-              console.log('**************************bodyContentA*******************');
-              console.log(bodyContentA);
-              console.log('**************************bodyContentA*******************');
             org.apexRest({ uri: 'contactUs', method:'POST', body: bodyContent }, function(err, result) {
-              console.log('**************************apexRest*******************');
-              console.log(err);
-              console.log(result);
-              console.log('**************************apexRest*******************');
               if(!err) {
-                console.log(result);
                 res.send("Thank you, your request has been processed");
               }else{
-                console.log(result);
                 res.send(result);
               }
             });
@@ -89,8 +66,6 @@ app.post('/form', function(req, res) {
         });
     });
 
-
-
     return;
 
 });
@@ -98,49 +73,7 @@ app.post('/form', function(req, res) {
 
 
 app.get('/', function(req, res) {
-  //if (isSetup()) {
-    /*var org = nforce.createConnection({
-      clientId: process.env.CONSUMER_KEY,
-      clientSecret: process.env.CONSUMER_SECRET,
-      redirectUri: oauthCallbackUrl(req),
-      mode: 'single'
-    });*/
-
-    //if (req.query.code !== undefined) {
-      //console.log('************************************');
-      //console.log('req is');
-      //console.log(req.query.code);
-      // authenticated
-
-      //oauthCode     = req.query;
-      //org.authenticate(oauthCode, function(err) {
-        //if (!err) {
-          //org.query({ query: 'SELECT id, name, type, industry FROM Account' }, function(err, results) {
-            //if (!err) {
-              res.render('index'); //, {records: results.records});
-            //}
-            //else {
-            //  res.send(err.message);
-            //}
-          //});
-        //}
-        //else {
-          /*if (err.message.indexOf('invalid_grant') >= 0) {
-            res.redirect('/');
-          }
-          else {
-            res.send(err.message);
-          }*/
-        //}
-      //});
-    /*}
-    else {
-      res.redirect(org.getAuthUri());
-    }*/
-  //}
-  //else {
-  //  res.redirect('/setup');
-  //}
+  res.render('index'); 
 });
 
 app.get('/setup', function(req, res) {
